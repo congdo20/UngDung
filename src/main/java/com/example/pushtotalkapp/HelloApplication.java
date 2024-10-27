b8d90a304d1b1018e4fd502e873d7382ab39a7ca
congdo20 <nguyencongdo20@gmail.com>
2024-10-27 12:21:17 +0700

    Demo pttapp

--- src/main/java/com/example/pushtotalkapp/HelloApplication.java
+++ src/main/java/com/example/pushtotalkapp/HelloApplication.java
@@ -1,21 +1,45 @@
 package com.example.pushtotalkapp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
+
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        primaryStage.setTitle("Hello!");
+        primaryStage.setTitle("Push To Talk Desktop");
+        BorderPane borderPane = new BorderPane();
+        Button btnPTT = new Button();
+        btnPTT.setText("Push To Talk");
+
+        Label label = new Label();
+        label.setText("Hehe");
+
+        borderPane.setCenter(btnPTT);
+        borderPane.setBottom(label);
+        //borderPane.setCenter(label);
+        //label.setAlignment(Pos.CENTER);
+
+
+        btnPTT.setOnAction(e -> {
+            label.setText("Talk To Me");
+            System.out.println("Talk To Me");
+        });
+
+
+        Scene scene = new Scene(borderPane, 200,200);
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
+
 
     public static void main(String[] args) {
         launch(args);
